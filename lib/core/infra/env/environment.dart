6572a3adc789b667fc76ae50d6f511fc2f65a14b
victor henzzo <victorhenzzowp@gmail.com>
2025-05-16 12:19:29 -0300

    feat: add environment class

---
+++ lib/core/infra/env/environment.dart
@@ -0,0 +1,10 @@
+class Environment {
+  const Environment();
+
+  String get weatherApiKey => const String.fromEnvironment(
+        'WEATHER_API_KEY',
+      );
+  String get weatherApiBaseUrl => const String.fromEnvironment(
+        'WEATHER_API_BASE_URL',
+      );
+}
