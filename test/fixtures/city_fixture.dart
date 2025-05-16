dd132f14d5211ec94241e4af130a7e72e9256369
victor henzzo <victorhenzzowp@gmail.com>
2025-05-16 09:00:22 -0300

    feat: add city fixture

---
+++ test/fixtures/city_fixture.dart
@@ -0,0 +1,39 @@
+import 'package:olly_olly_challenge/core/data/models/city_model.dart';
+import 'package:olly_olly_challenge/core/domain/entities/city_entity.dart';
+
+import 'position_fixture.dart';
+
+abstract class CityFixture {
+  static CityEntity entity() => CityEntity(
+        id: '3460960',
+        name: 'Itabira',
+        position: PositionFixture.entity(),
+        country: 'BR',
+        population: 99496,
+        timezone: -10800,
+        sunrise: 1747386846,
+        sunset: 1747427080,
+      );
+
+  static CityModel model() => CityModel(
+        id: '3460960',
+        name: 'Itabira',
+        position: PositionFixture.model(),
+        country: 'BR',
+        population: 99496,
+        timezone: -10800,
+        sunrise: 1747386846,
+        sunset: 1747427080,
+      );
+
+  static Map<String, dynamic> json() => {
+        'id': 3460960,
+        'name': 'Itabira',
+        'coord': PositionFixture.json(),
+        'country': 'BR',
+        'population': 99496,
+        'timezone': -10800,
+        'sunrise': 1747386846,
+        'sunset': 1747427080,
+      };
+}
