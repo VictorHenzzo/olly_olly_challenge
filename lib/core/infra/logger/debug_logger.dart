7f833acf9338429d6bf5184e6241102963442b6d
victor henzzo <victorhenzzowp@gmail.com>
2025-05-16 19:35:17 -0300

    fix: restrict logs to debug mode

--- lib/core/infra/logger/debug_logger.dart
+++ lib/core/infra/logger/debug_logger.dart
@@ -1,10 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:logger/logger.dart' as l;
 import 'package:olly_olly_challenge/core/infra/logger/logger.dart';
 
 @Injectable(as: Logger)
 class DebugLogger extends Logger {
-  DebugLogger() : _logger = l.Logger();
+  DebugLogger()
+      : _logger = l.Logger(
+          filter: _LoggerFilter(),
+        );
   final l.Logger _logger;
 
   @override
@@ -32,5 +36,12 @@ class DebugLogger extends Logger {
     final StackTrace? stackTrace,
   }) {
     _logger.w(message, error: error, stackTrace: stackTrace);
+  }
+}
+
+class _LoggerFilter extends l.LogFilter {
+  @override
+  bool shouldLog(final l.LogEvent event) {
+    return kDebugMode;
   }
 }
