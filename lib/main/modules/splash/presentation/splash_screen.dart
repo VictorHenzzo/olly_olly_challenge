4a52fe0bc75e9e73b8ff0613ac36c307e9914b58
victor henzzo <victorhenzzowp@gmail.com>
2025-05-16 23:38:03 -0300

    feat: add splash screen design

--- lib/main/modules/splash/presentation/splash_screen.dart
+++ lib/main/modules/splash/presentation/splash_screen.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/main/modules/splash/bloc/splash_bloc.dart';
+import 'package:weather_app/main/shared/ui_context.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({required this.bloc, super.key});
 
   final SplashBloc bloc;
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    widget.bloc.add(const InitializeSplashEvent());
+    super.initState();
+  }
+
+  @override
   Widget build(final BuildContext context) {
-    return const Placeholder();
+    final uiContext = UiContext.of(context);
+    final dimension = uiContext.shp(10);
+
+    return Scaffold(
+      body: Center(
+        child: SizedBox.square(
+          dimension: dimension,
+          child: CircularProgressIndicator(
+            strokeCap: StrokeCap.round,
+            strokeWidth: 10,
+            backgroundColor: uiContext.colorScheme.primary,
+            color: uiContext.colorScheme.onPrimary,
+          ),
+        ),
+      ),
+    );
   }
 }
