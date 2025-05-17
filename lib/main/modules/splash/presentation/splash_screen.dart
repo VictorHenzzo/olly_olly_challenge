4e20cc6c5794d87292b3b10859d43864f0cd40d7
victor henzzo <victorhenzzowp@gmail.com>
2025-05-16 23:47:49 -0300

    fix: undo commented businesse rule

--- lib/main/modules/splash/presentation/splash_screen.dart
+++ lib/main/modules/splash/presentation/splash_screen.dart
@@ -14,7 +14,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    // widget.bloc.add(const InitializeSplashEvent());
+    widget.bloc.add(const InitializeSplashEvent());
     super.initState();
   }
 
