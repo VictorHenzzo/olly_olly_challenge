e7f6aa6e49104b1654b88d6576c6f9b17a563f3e
victor henzzo <victorhenzzowp@gmail.com>
2025-05-17 13:34:22 -0300

    feat: add enum docs

--- lib/core/domain/enums/auth/auth_status.dart
+++ lib/core/domain/enums/auth/auth_status.dart
@@ -1,3 +1,6 @@
+/// Represents the authentication status of the user.
+///
+/// Used to indicate whether the user is currently authenticated or not.
 enum AuthStatus {
   authenticated,
   unauthenticated;
--- lib/core/domain/enums/location_access_permission.dart
+++ lib/core/domain/enums/location_access_permission.dart
@@ -1,3 +1,6 @@
+/// Represents the possible states of location access permission for the app.
+///
+/// Used to determine if the app can access the device's location services.
 enum LocationAccessPermission {
   denied,
   deniedForever,
